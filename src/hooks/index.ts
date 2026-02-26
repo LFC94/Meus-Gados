24e74a0094a2dbab34b455fbf9c45acfdaf7daaf
Lucas Felipe Costa <lucasfelipecost4@gmail.com>
2026-02-25 22:48:41 -0300

    Adiciona hook para gerenciar atualizações
    Disponibiliza o novo hook 'useUpdates' para que outras partes da aplicação possam integrar funcionalidades relacionadas a atualizações de forma padronizada e reativa.

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -4,3 +4,4 @@ export { useFormScreen } from "./use-form-screen";
 export { default as useNavigation } from "./use-navigation";
 export { default as useScreenHeader } from "./use-screen-header";
 export { default as useScreenOptions } from "./use-screen-options";
+export { useUpdates } from "./use-updates";
